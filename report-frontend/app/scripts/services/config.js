542ed2aea0ceda559483626fceef0ba89f898fb7
Frederik Reifschneider <reifschneider@karazy.net>
2014-04-04 14:09:15 +0200

    new prod build for version 0.4.1

--- report-frontend/app/scripts/services/config.js
+++ report-frontend/app/scripts/services/config.js
@@ -14,7 +14,7 @@ angular.module('PReports.services').provider('config', function() {
 	self.config_ = {
 		'serviceUrl' : '',
 		'servicePort' : '',
-		'version' : '0.4.0 Dude Diederich',
+		'version' : '0.4.1 Dude Diederich',
 		// 'serviceUrl' : '',
 		'priceRegExp' : /([0123456789]+)\.([0123456789]*)/,
 		'currencyFormats' : {
