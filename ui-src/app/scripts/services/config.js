d8d2eec179c073cd3d500561fc6c95ce58e89934
karazy <frederik.reifschneider@gmail.com>
2015-09-14 10:52:37 +0200

    optimized print css, new ui prod build

--- ui-src/app/scripts/services/config.js
+++ ui-src/app/scripts/services/config.js
@@ -12,8 +12,8 @@ angular.module('PReports.services').provider('config', function() {
 		@private
 	*/
 	self.config_ = {
-		'serviceUrl' : 'http://127.0.0.1',
-		'servicePort' : '3000',
+		'serviceUrl' : '',
+		'servicePort' : '',
 		'version' : '1.3.0 Grumpy Giraffe',
 		//the notification(emails) provider to use
 		'notificationProvider' : 'com-bisnode-notification',
