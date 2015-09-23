28775dc02547e5e4665ccbadfbdde477eb1a124b
karazy <frederik.reifschneider@gmail.com>
2015-09-23 08:53:22 +0200

    started to work on docker based backup including rotation

--- backup/backup.sh
+++ backup/backup.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 #Preports backup
 
 MONGO_PORT=27018
@@ -7,8 +8,11 @@ PREPORTS_ATTACHEMENTS=/opt/preports-data/upload/
 DB_BACKUP_PATH=$BACKUP_PATH/db
 ATT_BACKUP_PATH=$BACKUP_PATH/attachments
 
+BACKUP_DEST=/opt/preports-backup/incoming
+
 mkdir -p $DB_BACKUP_PATH;
 mkdir -p $ATT_BACKUP_PATH;
+mkdir -p BACKUP_DEST
 
 echo 'Creating dump of mongo db in ' $DB_BACKUP_PATH
 
@@ -16,12 +20,20 @@ echo 'Creating dump of mongo db in ' $DB_BACKUP_PATH
 #- | gzip > $BACKUP_PATH/db/preports_db.gz
 mongodump --host $MONGO_HOST --port $MONGO_PORT --db preports --out $DB_BACKUP_PATH
 
-echo 'Zipping mongo dump'
-
-tar -zcvf $DB_BACKUP_PATH/db.tar.gz $DB_BACKUP_PATH/preports
+#echo 'Zipping mongo dump'
+#tar -zcvf $DB_BACKUP_PATH/db.tar.gz $DB_BACKUP_PATH/preports
 
 echo 'Backing up report attachments'
 
-tar -zcvf $ATT_BACKUP_PATH/attachements.tar.gz $PREPORTS_ATTACHEMENTS
+cp -r $PREPORTS_ATTACHEMENTS $ATT_BACKUP_PATH/
+
+echo 'Zipping backup'
+tar -zcvf $BACKUP_DEST/backup.tar.gz $BACKUP_PATH
+
+#echo 'Calling backup rotation script'
+#sh rotate.sh
+
+#echo 'Cleanup'
+#rm -r $BACKUP_PATH
 
 echo 'Preports backup complete'
---
+++ backup/rotate.sh
@@ -0,0 +1,65 @@
+#!/bin/bash
+# Julius Zaromskis
+# changes by http://dev-eth0.de
+# Backup rotation
+
+# Storage folder where to move backup files
+# Must contain backup.monthly backup.weekly backup.daily folders
+storage=/your/backup/folder
+
+# Mail for notification if failed 
+TARGET_MAIL=foo@bar.de
+
+
+# Source folder where files are backed
+source=$storage/incoming
+
+# Destination file names
+date_daily=`date +"%Y-%m-%d"`
+
+# lifetime of backups before rotating in days
+LIFETIME_DAILY=14
+LIFETIME_WEEKLY=60
+LIFETIME_MONTHLY=300
+
+
+# Get current month and week day number
+month_day=`date +"%d"`
+week_day=`date +"%u"`
+
+# Create folders if required
+mkdir -p $storage/backup.daily
+mkdir -p $storage/backup.weekly
+mkdir -p $storage/backup.monthly
+
+# Check if there are any files in the backup folder
+test "$(ls -A $source)" || ((echo "Please check your backup" | mail $TARGET_MAIL -s "[Backup] Daily backup failed! Backup folder is empty.") && exit 1)
+
+# It is logical to run this script daily. We take files from source folder and move them to
+# appropriate destination folder
+
+# On first month day do
+if [ "$month_day" -eq 1 ] ; then
+  destination=backup.monthly/$date_daily
+else
+  # On saturdays do
+  if [ "$week_day" -eq 6 ] ; then
+    destination=backup.weekly/$date_daily
+  else
+    # On any regular day do
+    destination=backup.daily/$date_daily
+  fi
+fi
+
+# Move the files
+mkdir $destination
+mv -v $source/* $destination
+
+# daily
+find $storage/backup.daily/ -maxdepth 1 -mtime +$LIFETIME_DAILY -type d -exec rm -rv {} \;
+
+# weekly
+find $storage/backup.weekly/ -maxdepth 1 -mtime +$LIFETIME_WEEKLY -type d -exec rm -rv {} \;
+
+# monthly
+find $storage/backup.monthly/ -maxdepth 1 -mtime +$LIFETIME_MONTHLY -type d -exec rm -rv {} \;
