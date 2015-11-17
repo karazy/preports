fb005c978bb1d4c378c461770213306e17d7ffb9
karazy <frederik.reifschneider@gmail.com>
2015-11-17 22:43:21 +0100

    Added mocha chai and sinon npm packages for testing

---
+++ specs/reports.spec.js
@@ -0,0 +1,8 @@
+var reports = require('../routes/reports'),
+	chai = require('chai');
+
+describe('When deleting a report', function() {
+	it('should delete reports wiht missing files without exception', function() {
+		
+	});
+});
