5e460e2f70afe943430a7cb9d85946fa379bf23f
karazy <frederik.reifschneider@gmail.com>
2016-01-04 09:06:40 +0100

    UI dist build

--- client/app/scripts/services/config.js
+++ client/app/scripts/services/config.js
@@ -1,3 +1,4 @@
+'use strict';
 /** 
 *	@constructor
 * 	Provider for the 'config' service.
@@ -11,8 +12,8 @@ angular.module('PReports.services').provider('config', function() {
 		@private
 	*/
 	self.config_ = {
-		'serviceUrl' : 'http://127.0.0.1',
-		'servicePort' : '3000',
+		'serviceUrl' : '',
+		'servicePort' : '',
 		'version' : '1.5.0 Ignorant Ibis',
 		'priceRegExp' : /([0123456789]+)\.([0123456789]*)/,
 		'currencyFormats' : {
